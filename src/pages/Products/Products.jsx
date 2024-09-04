c20e6e81251966c78896973fcd294ab88ce6c995
incfDevuser <mgomeznavarrocorp@gmail.com>
2024-09-04 00:07:21 -0400

    .

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,34 +1,36 @@
 // src/components/Products.js
 import React, { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
-import axios from 'axios'
+import axios from 'axios';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
 
 const Products = ({ limite }) => {
   const [productos, setProductos] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null)
+  const [error, setError] = useState(null);
 
   //Hacer el seteado de los productos
-  useEffect(()=>{
-    const getProducts = async()=>{
+  useEffect(() => {
+    const getProducts = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/easycommerce/products');
+        const response = await axios.get('https://easy-back.onrender.com/easycommerce/products');
         const data = response.data;
-        setProductos(data)
+        setProductos(data);
       } catch (error) {
         setError(error.message);
-      }finally{
-        setLoading(false)
+      } finally {
+        setLoading(false);
       }
     };
     getProducts();
-  }, [])
+  }, []);
+  
   const productosAMostrar = limite ? productos.slice(0, limite) : productos;
+  
   if (loading) return <p>Cargando productos...</p>;
   if (error) return <p>{error}</p>;
+  
   return (
     <div>
       <div className="flex flex-wrap justify-center">
